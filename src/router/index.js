ba1f0d85cc2bc619b8fb11200f13dce485fd3682
tom_glb <goodMorning_glb@atguigu.com>
2019-11-09 15:47:38 +0800

    day01

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from 'views/home/Home'
 
 
 Vue.use(VueRouter)
 
-const routes = [
-
-]
+const routes = [{
+    path: "/home",
+    component: Home
+}]
 
 const router = new VueRouter({
     routes
---
+++ src/views/home/Home.vue
@@ -0,0 +1,49 @@
+<template>
+    <div id="home">  
+    <nav-bar class="home-nav">
+        <div slot="center"> 购物车</div>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <home-recommend-view :recommends="recommends"></home-recommend-view>
+    </div>
+</template>
+
+<script >
+import NavBar from 'components/common/navbar/NavBar';
+import {getHomeMultiData} from "network/home.js";
+import HomeSwiper from "./childComps/HomeSwiper"
+import HomeRecommendView from "./childComps/HomeRecommendView"
+
+export default {
+    name: "",
+    data() {
+        return {
+            result:null,
+            banners:[],
+            recommends:[]
+        }
+    },
+    components: {
+        NavBar,
+       HomeSwiper,
+       HomeRecommendView
+    },
+    created(){
+        //请求多个数据
+        getHomeMultiData().then(res=>{
+            console.log(res);
+            this.result=res;
+            this.banners=res.data.banner.list;
+            this.recommends=res.data.recommend.list;
+        })
+    },
+    methods: {}
+}
+</script>
+
+<style  scoped>
+
+.home-nav{
+    background-color: deeppink;
+}
+</style>
