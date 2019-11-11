2972ea46e1c28f1e3447db19b7f90c31332d8fc7
Hongchao_Qin <goodMorning_glb@atguigu.com>
2019-11-11 21:52:26 +0800

    day043

--- src/network/detail.js
+++ src/network/detail.js
@@ -1,12 +1,35 @@
-import {request} from "./request.js"
-export function getDetailById(iid){
-    return request(
-        {
-            url:'/detail',
-            params:{
+import { request } from "./request.js"
+export function getDetailById(iid) {
+    return request({
+            url: '/detail',
+            params: {
                 iid
-            } 
+            }
         }
-       
+
     );
+}
+
+export class Goods {
+    constructor(itemInfo, columns, services) {
+        this.title = itemInfo.title;
+        this.desc = itemInfo.desc;
+        this.newPrice = itemInfo.price;
+        this.oldPrice = itemInfo.oldPrice;
+        this.discount = itemInfo.discountDesc;
+        this.columns = columns;
+        this.services = services;
+        this.reslPrice = itemInfo.lowNowPrice;
+    }
+}
+
+export class Shop {
+    constructor(shopInfo) {
+        this.log = shopInfo.shopLogo;
+        this.name = shopInfo.name;
+        this.fans = shopInfo.cFans;
+        this.sells = shopInfo.cSells;
+        this.score = shopInfo.score;
+        this.goodsCount = shopInfo.cGoods;
+    }
 }
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,33 +2,41 @@
     <div>
        <detail-nav-bar></detail-nav-bar>
        <detail-swiper :topImages="topImages"></detail-swiper>
+       <detail-base-info :goods="goods"></detail-base-info>
     </div>
 </template>
 
 <script>
 import DetailNavBar from './childComps/DetailNavBar'
-import {getDetailById} from 'network/detail.js'
+import {getDetailById,Goods,Shop} from 'network/detail.js'
 import DetailSwiper from './childComps/DetailSwiper'
+import DetailBaseInfo from './childComps/DetailBaseInfo'
 
 export default {
     name:"Detail",
     data() {
         return {
             iid:"",
-            topImages:[]
+            topImages:[],
+            goods:null,
+            shop:null
         };
     },
     mounted(){
        
         this.iid=this.$route.params.iid;
         getDetailById(this.iid).then((res)=>{
+            console.log(res);
             this.topImages=res.result.itemInfo.topImages;
+            this.goods=new Goods(res.result.itemInfo,res.result.columns,res.result.shopInfo.services);
+            this.shop=new Shop(res.result.shopInfo);
         });
 
     },
     components:{
         DetailNavBar,
-        DetailSwiper
+        DetailSwiper,
+        DetailBaseInfo
     }
 };
 </script>
